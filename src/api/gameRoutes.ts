0fda57799a120207dc47d8df462062aa8002b84f
Gavin <46635587+blue2cat@users.noreply.github.com>
2025-02-28 19:46:11 -0800

    Build first pass at game logic

--- src/api/gameRoutes.ts
+++ src/api/gameRoutes.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
-import Board from '../interfaces/board';
-import { randomBoard, zeroedBoard } from './helpers';
+import { randomBoard, validateBoard, zeroedBoard } from './helpers';
 const _ = require('lodash');
+
 // Build the API router
 const gameRouter = Router();
 
@@ -12,21 +12,8 @@ gameRouter.get("/randomBoard", (req: Request, res: Response) => {
 
 // GET a 4x4 grid of characters - /submitRoll - verify that the submitted grid is valid and return the list of valid words
 gameRouter.post("/submitRoll", (req: Request, res: Response) => {
-  const reqBoard: Board = req.body;
-  let visited: Board = zeroedBoard();
-
-  
-  let validWords: Array<string> = [];
-
-
-
-  console.log("Board received: ");
-  console.log(reqBoard);
-
-
-
   res.header("Content-Type", "application/json");
-  res.json("a");
+  res.json(validateBoard(req.body));
 })
 
 export default gameRouter;
--- src/api/helpers.ts
+++ src/api/helpers.ts
@@ -26,5 +26,57 @@ function zeroedBoard(): Board {
   return newBoard;
 }
 
+// validate a board and return json object
+function validateBoard(reqBoard: Board): Array<string> {
+
+  // Create a visited board to keep track of visited cells we have performed DFS on
+  let visited: Board = zeroedBoard();
+  let validWords: Array<string> = [];
+
+  // Loop through all the spaces on the board and perform a DFS search on each
+  for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < 4; y++) {
+      // Start at the current space on the board and iterate outward in all possible directions
+      validWords = validWords.concat(performDFS(reqBoard, visited, x, y, reqBoard.grid[x][y]));
+    }
+  }
+  return validWords;
+}
+
+// perform a DFS search on a given position on the board
+function performDFS(board: Board, visited: Board, x: number, y: number, subWord: string): Array<string> {
+  let validWords: Array<string> = [];
+
+  // Mark that we have performed our search starting at position [i][j]  
+  visited.grid[x][y] = "1";
+
+  // Attempt to build an incrementally larger word from all spaces around the current character
+  subWord = subWord + board.grid[x][y];
+
+  // Check if the current candidate word is actually a word, if so, add to the valid list
+  if (isValidWord(subWord)) {
+    validWords = validWords.concat(subWord);
+  }
+
+  // Recursively call ourself on each adjacent cell
+  for (let i = x - 1; i <= x + 1; i++) {
+    for (let j = y - 1; j <= y + 1; j++) {
+      if (i >= 0 && i < 4 && j >= 0 && j < 4 && visited.grid[i][j] == "0") {
+        validWords = validWords.concat(performDFS(board, visited, i, j, subWord));
+      }
+    }
+  }
+
+  visited.grid[x][y] = "0";
+
+  return validWords;
+}
+
+// return true if the given string is in the dictionary of valid words
+function isValidWord(word: string): Boolean {
+  console.log("checking" + word);
+  return _.includes(global.words, word);
+}
+
 // export the functions
-export { randomBoard, zeroedBoard, validChars };
+export { randomBoard, zeroedBoard, validChars, validateBoard };
